cd2113ab93a84d7345f6e2b0a1b9fb7de47a7649
lekhuongduy <lekhuongduy1998@gmail.com>
2021-09-01 15:57:55 +0700

    servicefee

--- src/main/java/net/javaguides/springboot/controller/ServiceFreeController.java
+++ src/main/java/net/javaguides/springboot/controller/ServiceFreeController.java
@@ -6,7 +6,6 @@ import net.javaguides.springboot.repository.ServiceFeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,16 +31,6 @@ public class ServiceFreeController {
     public ServiceFee createServiceFee(@RequestBody ServiceFee serviceFee){
         return serviceFeeRepository.save(serviceFee);
     }
-
-//    @PutMapping("servicefee/{id}")
-//    public ServiceFee updateServiceFee(@PathVariable int id, @RequestBody ServiceFee serviceFee){
-//        ServiceFee serviceFee1 = serviceFeeRepository.findById(id).
-//                orElseThrow(() -> new RequestRejectedException("service not exist with id" + id));
-//        serviceFee1.setNameService(serviceFee.getNameService());
-//        serviceFee1.setPrice(serviceFee.getPrice());
-//
-//        return serviceFeeRepository.save(serviceFee1);;
-//    }
 
     @PutMapping("/servicefee/update")
     public ResponseEntity updateServiceFee(@RequestBody ServiceFee serviceFee){
